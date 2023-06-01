e2a8da074bcfb3fc360788111548e2b5d3c9a3a2
dialite <raymondolisa01@gmail.com>
2023-06-01 11:58:11 +0100

    Import different components from google maps api and material ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { Box, InputBase, Toolbar, Typography, AppBar } from "@mui/material";
 
 const Header = () => {
   return <h1>Header</h1>;
