dba42b0356550a98864292180b4ab53062223448
dialite <raymondolisa01@gmail.com>
2023-06-02 22:07:20 +0100

    Create map container and style it in map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+// import { Paper, Typography, useMediaQuery } from "@mui/material";
+// import { LocationOnOutlinedIcon } from "@mui/icons-material/LocationOnOutlined";
+// import Rating from "@mui/material";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  // const isMobile = useMediaQuery("(min-width: 600px)");
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCOGPVezLBbekVKMZKjzAPlXf-FRWyo3M8" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        // options={""}
+        // onChange={""}
+        // onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
